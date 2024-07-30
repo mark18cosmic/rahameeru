a73ca2dfc96edfcb3473d598831b1404e4d05be8
mark18cosmic <kaish018@gmail.com>
2024-07-31 02:26:59 +0500

    fix: carousel

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -3,9 +3,10 @@
 import Image from 'next/image';
 import React from 'react';
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import './carouselstyles.css';
 import Banner1 from '/public/banner/mgg-vitchakorn-vBOxsZrfiCw-unsplash.jpg'
 import Banner2 from '/public/banner/scott-webb-Pf6Mw9RTDUI-unsplash.jpg'
+import Banner3 from '/public/banner/alex-haney-CAhjZmVk5H4-unsplash.jpg'
 
 interface CarouselProps {
   images: string[];
@@ -13,9 +14,10 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ images }) => {
   return (
-    <ResponsiveCarousel showArrows={false} autoPlay={true} infiniteLoop={true}>
-      <Image src={Banner1} alt='image'/>
-      <Image src={Banner2} alt='image'/>
+    <ResponsiveCarousel showArrows={false} autoPlay={true} infiniteLoop={true} className='roundedcarousel'>
+      <Image src={Banner1} alt='image' />
+      <Image src={Banner2} alt='image' />
+      <Image src={Banner3} alt='image' />
     </ResponsiveCarousel>
   );
 };
