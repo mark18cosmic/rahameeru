c697214593f9d81ffab0d22849bf3c26c4df1484
mark18cosmic <kaish018@gmail.com>
2024-08-12 23:17:08 +0500

    fix: find in maps button

--- app/components/buttons/MapButton.tsx
+++ app/components/buttons/MapButton.tsx
@@ -2,8 +2,11 @@ import { Button } from '@nextui-org/react'
 import React from 'react'
 import { TiLocationArrowOutline } from "react-icons/ti";
 const MapButton = () => {
-  return (
-    <Button className='bg-white border font-semibold flex flex-row items-center'><TiLocationArrowOutline size={30}/> Find in Maps</Button>
+    return (
+        <div className='md:w-full'>
+            <Button className='bg-white border md:text-large font-semibold flex flex-row items-center'><TiLocationArrowOutline size={25} /> Find in Maps</Button>
+
+        </div>
     )
 }
 
