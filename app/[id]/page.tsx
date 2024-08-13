310133663dd64388327b5ebf85ddafed2f581db8
mark18cosmic <kaish018@gmail.com>
2024-08-14 02:34:30 +0500

    feat: dynamic meta tags

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -13,6 +13,7 @@ import MapButton from '../components/buttons/MapButton';
 import Review, { RatingIcon } from '../components/buttons/Review';
 import { HiOutlineLocationMarker } from 'react-icons/hi';
 import ReviewList from '../components/Review/ReviewList';
+import Head from 'next/head';
 
 // Fetch data for the specific restaurant
 export default async function RestaurantDetail({ params }: { params: RestaurantParams }) {
@@ -25,41 +26,45 @@ export default async function RestaurantDetail({ params }: { params: RestaurantP
     }
 
     return (
-        <main className="m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black">
-            <div className='flex items-center justify-center'>
-                <Image src={restaurant.image} alt={restaurant.label} width={500} height={500} className='rounded-lg object-cover' />
-            </div>
-            <div className='flex flex-row items-center justify-between'>
-                <div>
-                    {restaurant.badges.map((badge) => (
-                        <Badge key={badge} label={badge} />
-                    ))}{" "}
+        <>
+            <Head>
+                <title>{restaurant.label} | RahaMeeru</title>
+            </Head>
+            <main className="m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black">
+                <div className='flex items-center justify-center'>
+                    <Image src={restaurant.image} alt={restaurant.label} width={500} height={500} className='rounded-lg object-cover' />
+                </div>
+                <div className='flex flex-row items-center justify-between'>
+                    <div>
+                        {restaurant.badges.map((badge) => (
+                            <Badge key={badge} label={badge} />
+                        ))}{" "}
+                    </div>
+                    <div>
+                        <RatingIcon key={''} label={''} ratings={restaurant.ratings} image={''} location={''} desc={''} badges={[]} />
+                    </div>
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <div>
+                        <h3 className='text-xl md:text-2xl font-semibold'>{restaurant.label}</h3>
+                    </div>
+                    <div>
+                        <p className='font-light'>{restaurant.desc}</p>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center justify-between'>
+                    <div>
+                        <p className='flex items-center gap-1 flex-row'><HiOutlineLocationMarker /> {restaurant.location}</p>
+                    </div>
+                    <div className='flex flex-row gap-2 md:gap-4'>
+                        <MapButton />
+                        <Review />
+                    </div>
                 </div>
                 <div>
-                    <RatingIcon key={''} label={''} ratings={restaurant.ratings} image={''} location={''} desc={''} badges={[]} />
+                    <ReviewList />
                 </div>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <div>
-                    <h3 className='text-xl md:text-2xl font-semibold'>{restaurant.label}</h3>
-                </div>
-                <div>
-                    <p className='font-light'>{restaurant.desc}</p>
-                </div>
-            </div>
-            <div className='flex flex-row items-center justify-between'>
-                <div>
-                    <p className='flex items-center gap-1 flex-row'><HiOutlineLocationMarker /> {restaurant.location}</p>
-                </div>
-                <div className='flex flex-row gap-2 md:gap-4'>
-                    <MapButton />
-                    <Review />
-                </div>
-            </div>
-            <div>
-                <ReviewList />
-            </div>
-        </main>
+            </main></>
     );
 }
 
