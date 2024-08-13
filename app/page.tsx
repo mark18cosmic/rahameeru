915f79ee12b462612ff0a211059bc2480ba72b44
mark18cosmic <kaish018@gmail.com>
2024-08-14 02:53:37 +0500

    failed seo

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,6 @@ import { NextSeo } from "next-seo";
 export default function Home() {
   return (
     <>
-    <NextSeo 
-      title="Rahameeru | Find the best places to dine near you"
-      description="Find the best restaurants and review your favorite ones by simply creating an account on rahameeru. With an extensive library full of restaurants in Male and Hulhumale find the cheapest or most luxurious or even the best date spots now!"
-    />
       <div>
         <Carousel />
         <main className="m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black">
