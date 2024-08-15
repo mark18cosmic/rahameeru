6d438f65a6d1683d9418cb69841117f3c2cdcd5d
mark18cosmic <kaish018@gmail.com>
2024-08-15 20:48:53 +0500

    wip: authentication components

--- app/components/authentication/Signup.tsx
+++ app/components/authentication/Signup.tsx
@@ -5,26 +5,29 @@ import { signUp } from '@/app/api/auth/signup';
 import { Button, Input } from '@nextui-org/react';
 
 export const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
 
-  const handleSignUp = async () => {
-    try {
-      await signUp(email, password);
-      alert('Sign-up successful!');
-    } catch (error) {
-      console.error(error);
-      alert('Error during sign-up');
-    }
-  };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  return (
-    <div className='flex flex-col gap-3'>
-      <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <Input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <Button onClick={handleSignUp} className='bg-root-500  md:text-large font-semibold flex flex-row items-center'>Signup</Button>
-    </div>
-  );
+    const handleSignUp = async () => {
+        try {
+            await signUp(email, password);
+            alert('Sign-up successful!');
+        } catch (error) {
+            console.error(error);
+            alert('Error during sign-up');
+        }
+    };
+
+    return (
+        <div className='flex flex-col gap-3'>
+            <Input type="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <Input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <Input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <Button onClick={handleSignUp} className='bg-root-500 text-white  md:text-large font-semibold flex flex-row items-center'>Signup</Button>
+        </div>
+    );
 };
 
 export default SignUp;
