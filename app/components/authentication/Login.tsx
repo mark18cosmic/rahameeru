737e7042364e7bf5da1816853af5bd4c8146ca96
mark18cosmic <kaish018@gmail.com>
2024-08-15 20:30:31 +0500

    fix: auth client

--- app/components/authentication/Login.tsx
+++ app/components/authentication/Login.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react';
 import { logIn } from '@/app/api/auth/login';
 
--- app/components/authentication/Signup.tsx
+++ app/components/authentication/Signup.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react';
 import { signUp } from '@/app/api/auth/signup';
 
