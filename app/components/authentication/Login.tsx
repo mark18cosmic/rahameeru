3196cc20f0c776058793404917570bfd214708b5
mark18cosmic <kaish018@gmail.com>
2024-08-17 02:19:46 +0500

    fix: password label

--- app/components/authentication/Login.tsx
+++ app/components/authentication/Login.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { logIn } from '@/app/api/auth/login';
-import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react';
+import { Button, Card, CardBody, CardHeader, Input, Link } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 
 
@@ -31,7 +31,15 @@ const SignIn = () => {
       <CardBody>
         <div className='flex flex-col gap-3'>
           <Input variant='bordered' labelPlacement='outside' label="Email" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-          <Input variant='bordered' labelPlacement='outside' label="Password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <div>
+            <div>
+              <label>Password</label>
+              <Link className='text-root-500'>
+                Forgot Password
+              </Link>
+            </div>
+            <Input variant='bordered' type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </div>
           <Button onClick={handleSignIn} className='bg-root-500 text-white  md:text-large font-semibold flex flex-row items-center'>Login</Button>
         </div>
       </CardBody>
