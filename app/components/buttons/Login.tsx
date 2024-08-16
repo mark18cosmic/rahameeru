650e6243dba1127ae0571c93748a53a45d2a0bb7
mark18cosmic <kaish018@gmail.com>
2024-08-16 22:51:22 +0500

    fix: button border and text colors

--- app/components/buttons/Login.tsx
+++ app/components/buttons/Login.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AuthButtons = () => {
     return (
         <div className='md:w-full flex flex-row gap-2'>
-            <Button className='bg-white border md:text-large font-semibold flex flex-row items-center'>Login</Button>
+            <Button className='bg-white border border-black text-black md:text-large font-semibold flex flex-row items-center'>Login</Button>
             <Button className='bg-root-500 text-white md:text-large font-semibold flex flex-row items-center'>Sign up</Button>
         </div>)
 }
--- app/components/buttons/MapButton.tsx
+++ app/components/buttons/MapButton.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from "react-icons/fa";
 const MapButton = () => {
     return (
         <div className='md:w-full'>
-            <Button className='bg-white border md:text-large font-semibold flex flex-row items-center'>Find in Maps <FaLocationArrow /></Button>
+            <Button className='bg-white border-black border text-black md:text-large font-semibold flex flex-row items-center'>Find in Maps <FaLocationArrow /></Button>
 
         </div>
     )
