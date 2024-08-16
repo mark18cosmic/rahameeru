e95ace397030c5795a5992358d6cd5b8d5782db5
mark18cosmic <kaish018@gmail.com>
2024-08-17 01:01:43 +0500

    s

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import SignIn from '../components/authentication/Login'
 const LoginPage = () => {
   return (
     <>
-    <main className='m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black min-h-screen'>
+    <main className='m-4 md:m-6 flex justify-center flex-col gap-5 md:gap-8 text-black min-h-screen'>
         <div className='flex justify-center items-center'>
             <SignIn />
         </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,7 +4,7 @@ import SignUp from '../components/authentication/Signup'
 const SignupPage = () => {
   return (
     <>
-    <main className='m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black'>
+    <main className='m-4 md:m-6 flex flex-col justify-center min-h-screen gap-5 md:gap-8 text-black'>
         <div className='flex justify-center items-center'>
             <SignUp />
         </div>
