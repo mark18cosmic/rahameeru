a083711c22d1244b3a222fb0eed0d065ac19777f
mark18cosmic <kaish018@gmail.com>
2024-08-17 03:26:23 +0500

    save me

--- app/components/authentication/googleSignin.tsx
+++ app/components/authentication/googleSignin.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { signInWithGoogle } from '@/app/firebase/firebaseConfig'; // Make sure to import the function correctly
 
 const GoogleSignIn = () => {
