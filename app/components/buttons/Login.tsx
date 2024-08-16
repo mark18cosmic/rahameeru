5084e416a9200a66518a5eb2679b6ce93a17432d
mark18cosmic <kaish018@gmail.com>
2024-08-16 23:54:23 +0500

    feat: working auth buttons

--- app/components/buttons/Login.tsx
+++ app/components/buttons/Login.tsx
@@ -1,11 +1,16 @@
 import { Button } from '@nextui-org/react'
+import Link from 'next/link'
 import React from 'react'
 
 const AuthButtons = () => {
     return (
         <div className='md:w-full flex flex-row gap-2'>
-            <Button className='bg-white border border-black text-black md:text-large font-semibold flex flex-row items-center'>Login</Button>
-            <Button className='bg-root-500 text-white md:text-large font-semibold flex flex-row items-center'>Sign up</Button>
+            <Link href={"/login"}>
+                <Button className='bg-white border border-black text-black md:text-large font-semibold flex flex-row items-center'>Login</Button>
+            </Link>
+            <Link href={"/signup"}>
+                <Button className='bg-root-500 text-white md:text-large font-semibold flex flex-row items-center'>Sign up</Button>
+            </Link>
         </div>)
 }
 
