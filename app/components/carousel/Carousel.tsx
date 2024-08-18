dab746addbe772368fe4bdc77c7ae92727345033
mark18cosmic <kaish018@gmail.com>
2024-08-18 17:04:40 +0500

    jkj

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { Carousel } from "@material-tailwind/react";
- 
+
 export function CarouselDefault() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel className="rounded-xl" autoplay={true} // Optional: autoplay feature
+      loop={true} // Optional: infinite loop
+      transition={{ duration: 1 }}>
       <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
