a32319a6d74b60eca6f077acf892e9a9bde6a89d
mark18cosmic <kaish018@gmail.com>
2024-08-18 17:16:28 +0500

    jklh

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
-// import './carouselstyles.css';
+import './carouselstyles.css';
 import Banner1 from '/public/banner/mgg-vitchakorn-vBOxsZrfiCw-unsplash.jpg'
 import Banner2 from '/public/banner/scott-webb-Pf6Mw9RTDUI-unsplash.jpg'
 import Banner3 from '/public/banner/alex-haney-CAhjZmVk5H4-unsplash.jpg'
