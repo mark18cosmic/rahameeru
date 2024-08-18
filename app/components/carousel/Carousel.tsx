44793383c7b1ccfeb1af0ef3dfa20fc409517a6b
mark18cosmic <kaish018@gmail.com>
2024-08-18 16:56:32 +0500

    dsfa

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -1,22 +1,23 @@
-"use client"
-
-import Image from 'next/image';
-import React from 'react';
-import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
-// import './carouselstyles.css';
-import Banner1 from '/public/banner/mgg-vitchakorn-vBOxsZrfiCw-unsplash.jpg'
-import Banner2 from '/public/banner/scott-webb-Pf6Mw9RTDUI-unsplash.jpg'
-import Banner3 from '/public/banner/alex-haney-CAhjZmVk5H4-unsplash.jpg'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-
-const Carousel: React.FC = () => {
+import { Carousel } from "@material-tailwind/react";
+ 
+export function CarouselDefault() {
   return (
-    <ResponsiveCarousel showArrows={false} autoPlay={true} infiniteLoop={true} className='rounded-xl'>
-      <Image src={Banner1} alt='image' className='md:h-[450px] h-[350px] object-cover' />
-      <Image src={Banner2} alt='image' className='md:h-[450px] h-[350px] object-cover' />
-      <Image src={Banner3} alt='image' className='md:h-[450px] h-[350px] object-cover' />
-    </ResponsiveCarousel>
+    <Carousel className="rounded-xl">
+      <img
+        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        alt="image 1"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        alt="image 2"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+    </Carousel>
   );
-};
-
-export default Carousel
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,33 @@
 import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
-const config: Config = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const config: Config = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-
-    },
+    extend: {},
     colors: {
-      'root-100': '#FFE4E1',
-      'root-200': '#FFCCC7',
-      'root-300': '#FFA8A0',
-      'root-400': '#FF7D71',
-      'root-500': '#F84B3B',
-      'root-600': '#E52E1D',
-      'root-700': '#C12314',
-      'root-800': '#A02014',
-      'white': '#FFFFFF',
-      'gray': '#D4D4D8',
-      'black': '#333333:',
-    }
+      "root-100": "#FFE4E1",
+      "root-200": "#FFCCC7",
+      "root-300": "#FFA8A0",
+      "root-400": "#FF7D71",
+      "root-500": "#F84B3B",
+      "root-600": "#E52E1D",
+      "root-700": "#C12314",
+      "root-800": "#A02014",
+      white: "#FFFFFF",
+      gray: "#D4D4D8",
+      black: "#333333:",
+    },
   },
   plugins: [nextui()],
-};
+});
 export default config;
