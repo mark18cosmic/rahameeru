44f339588903c8e8b3bb5bea2e1d6e8df5f7ea2c
mark18cosmic <kaish018@gmail.com>
2024-09-10 21:35:40 +0500

    fix: text sizings

--- app/components/restaurantCard/restaurantCard.tsx
+++ app/components/restaurantCard/restaurantCard.tsx
@@ -28,13 +28,13 @@ const RestaurantCard: React.FC<RestaurantProps> = ({ label, ratings, image, loca
                         </div>
                         <div className='flex flex-col my-2'>
                             <div className='flex flex-row mt-1 p-1 gap-2 items-center justify-between mb-2'>
-                                <h2 className="font-semibold md:text-3xl text-xl">{label}</h2>
+                                <h2 className="font-semibold md:text-xl text-large">{label}</h2>
                                 <p className="text-base md:text-large flex flex-row items-center text-root-500">
                                     {ratings} <FaStar />
                                 </p>
                             </div>
                             <div className='flex flex-row justify-between'>
-                                <span className='flex flex-row items-center gap-1 md:text-medium text-sm'>
+                                <span className='flex flex-row items-center gap-1 md:text-base text-sm'>
                                     <HiOutlineLocationMarker /> {location}
                                 </span>
                             </div>
