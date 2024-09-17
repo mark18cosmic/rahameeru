5a83834020e7fd36c7e1db1e60fd8af1350b89b0
mark18cosmic <kaish018@gmail.com>
2024-09-18 00:49:49 +0500

    new logo intialisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/public/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
+        <link rel="manifest" href="/public/site.webmanifest"></link>
       </head>
       <body className={inter.className}>
         <NextUIProvider>
