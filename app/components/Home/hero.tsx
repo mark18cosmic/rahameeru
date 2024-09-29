66f922fa13f2d2e8d4384081f7fd1e215c7ce554
mark18cosmic <kaish018@gmail.com>
2024-09-29 22:59:03 +0500

    sdfgasdf

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <section className="bg-white">
             <div className="flex justify-between max-w-screen-xl px-4 py-8 m-4 md:m-6 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 {/* Left side: Heading and Buttons */}
-                <div className="w-1/2 place-self-center lg:col-span-7">
+                <div className="md:w-1/2 place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
                         <span className='text-root-800'>Discover </span>the Best Restaurants in the Maldives
                     </h1>
@@ -32,7 +32,7 @@ const Hero = () => {
                 </div>
 
                 {/* Right side: Image */}
-                <div className="hidden w-1/2 lg:mt-0 lg:col-span-5 rounded-xl lg:flex">
+                <div className="hidden md:w-1/2 lg:mt-0 lg:col-span-5 rounded-xl lg:flex">
                     <Image src={HeroImage} alt="restaurant mockup" />
                 </div>
             </div>
