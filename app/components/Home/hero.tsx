a96bb469c59ca5f3a2ca1fb6be759a8a505dfdf4
mark18cosmic <kaish018@gmail.com>
2024-09-29 22:57:02 +0500

    asd

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <section className="bg-white">
             <div className="flex justify-between max-w-screen-xl px-4 py-8 m-4 md:m-6 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 {/* Left side: Heading and Buttons */}
-                <div className="mr-auto place-self-center lg:col-span-7">
+                <div className="w-1/2 place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
                         <span className='text-root-800'>Discover </span>the Best Restaurants in the Maldives
                     </h1>
@@ -21,17 +21,18 @@ const Hero = () => {
                     </p>
                     <div className='flex gap-2 md:gap-4'>
                         <Button
+                            href={'/signup'}
                             className="bg-root-500 text-white md:text-base text-sm font-semibold flex flex-row items-center"
                         >Get started<FaArrowRight /></Button>
 
-                        <Button className='bg-white border-black border text-black text-sm md:text-base font-semibold flex flex-row items-center'>Contact us</Button>
+                        <Button className='bg-white border-black border text-black text-sm md:text-base font-semibold flex flex-row items-center' href="/contact-us">Contact us</Button>
                     </div>
 
 
                 </div>
 
                 {/* Right side: Image */}
-                <div className="hidden lg:mt-0 lg:col-span-5 rounded-xl lg:flex">
+                <div className="hidden w-1/2 lg:mt-0 lg:col-span-5 rounded-xl lg:flex">
                     <Image src={HeroImage} alt="restaurant mockup" />
                 </div>
             </div>
