da64cab6ba16a7f27b2d4826cca2c2c518a3f029
mark18cosmic <kaish018@gmail.com>
2024-09-29 22:49:58 +0500

    asdf

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -10,7 +10,7 @@ import { FaPhoneAlt } from 'react-icons/fa';
 const Hero = () => {
     return (
         <section className="bg-white">
-            <div className="grid max-w-screen-xl px-4 py-8 m-4 md:m-6 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="flex justify-between max-w-screen-xl px-4 py-8 m-4 md:m-6 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 {/* Left side: Heading and Buttons */}
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
