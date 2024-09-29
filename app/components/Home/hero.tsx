afa72f9bb504655eec9f72d93dfc7a83d4983854
mark18cosmic <kaish018@gmail.com>
2024-09-29 22:44:51 +0500

    asfd

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -13,8 +13,8 @@ const Hero = () => {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 {/* Left side: Heading and Buttons */}
                 <div className="mr-auto place-self-center lg:col-span-7">
-                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-root-500">
-                        Discover the Best Restaurants in the Maldives
+                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+                        <span className='text-root-800'>Discover</span>the Best Restaurants in the Maldives
                     </h1>
                     <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
                         Rahameeru helps you explore top-rated dining spots, read authentic reviews, and share your experiences. Join us in discovering the best places to eat around the islands!
