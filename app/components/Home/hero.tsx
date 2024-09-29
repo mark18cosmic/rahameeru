a05148b5a3eb9a1c914478d8bd231d47e7bc0ec4
mark18cosmic <kaish018@gmail.com>
2024-09-30 00:31:21 +0500

    automated commit message

--- app/components/Home/hero.tsx
+++ app/components/Home/hero.tsx
@@ -58,9 +58,6 @@ const Hero = () => {
                         <div>
                             <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
                         </div>
-                        <div>
-                            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
-                        </div>
                     </div>
                 </div>
             </div>
