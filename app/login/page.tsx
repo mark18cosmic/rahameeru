04a26c5d3f14a7d952530d4b8f0b096d4408aebc
mark18cosmic <kaish018@gmail.com>
2024-09-29 08:59:59 +0500

    asda

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
     <>
       {/* <Navbar /> */}
       <main className='h-full m-4 md:m-6 flex flex-col gap-5 md:gap-8 text-black'>
-        <div className='flex min-h-full items-center justify-center p-5'>
+        <div className='flex mb-14 items-center justify-center p-5'>
           <SignIn />
         </div>
       </main>
