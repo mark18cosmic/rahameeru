bfd2f243e1667dede769755e84f859bf0169ef47
mark18cosmic <kaish018@gmail.com>
2024-09-29 23:54:48 +0500

    automated commit message

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@nextui-org/react'
+import Link from 'next/link'
 import React from 'react'
 
 function notFound() {
@@ -9,7 +10,9 @@ function notFound() {
                 <h1 className="mt-4 text-3xl font-bold tracking-tight text-black sm:text-5xl">Page not found</h1>
                 <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <Button className='bg-root-500 text-white md:text-base text-sm font-semibold flex flex-row items-center' href={'/'}>Go back</Button>
+                    <Link href={'..'}>
+                        <Button className='bg-root-500 text-white md:text-base text-sm font-semibold flex flex-row items-center'>Go back</Button>
+                    </Link>
                 </div>
             </div>
         </main>
