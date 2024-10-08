c3268908601f016b54dcb4757a156278dfe52ed9
mark18cosmic <kaish018@gmail.com>
2024-10-08 07:25:48 +0500

    automated commit message

--- app/components/Home/Search.tsx
+++ app/components/Home/Search.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
 import { getRestaurantsData } from '@/app/utils/getRestaurantData'; // Import your function
