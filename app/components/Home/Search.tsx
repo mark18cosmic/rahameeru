996306af07edc0d39dd127e04c729dfd3d4df0cf
mark18cosmic <kaish018@gmail.com>
2024-10-08 07:45:49 +0500

    automated commit message

--- app/components/Home/Search.tsx
+++ app/components/Home/Search.tsx
@@ -1,11 +1,9 @@
-'use client'
-
 import React, { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
 import { getRestaurantsData } from '@/app/utils/getRestaurantData'; // Import your function
 import { RestaurantProps } from '../restaurantCard/restaurantCard'; // Adjust the path
 import { Input } from '@nextui-org/react';
-import { Listbox, ListboxItem } from "@nextui-org/listbox";
+
 
 export const Search: React.FC = () => {
     const [query, setQuery] = useState('');
@@ -28,11 +26,6 @@ export const Search: React.FC = () => {
         includeScore: true,
         threshold: 0.3, // Sensitivity of the search
     });
-    const ListboxWrapper = ( {children} : any ) => (
-        <div className="w-full max-w-[960px] border-small px-1 py-2 rounded-small border-default-200 ">
-            {children}
-        </div>
-    );
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         const searchQuery = e.target.value;
@@ -57,33 +50,31 @@ export const Search: React.FC = () => {
                 className=""
             />
 
-            <ListboxWrapper>
-                <Listbox className="mt-4">
-                    {results.length > 0 ? (
-                        <div className="bg-white shadow-lg rounded-lg p-4 space-y-2">
-                            {results.map((restaurant) => (
-                                <ListboxItem
-                                    key={restaurant.label}
-                                    href={`/${restaurant.label.replace(/\s+/g, '-').toLowerCase()}`}
-                                    className="block bg-gray-100 hover:bg-gray-200 transition-colors duration-300 p-2 rounded-md"
-                                >
-                                    <div className="flex items-center gap-3">
-                                        <img
-                                            src={restaurant.image}
-                                            alt={restaurant.label}
-                                            className="w-10 h-10 object-cover rounded-full"
-                                        />
-                                        <span className="font-semibold text-gray-800">
-                                            {restaurant.label}
-                                        </span>
-                                    </div>
-                                </ListboxItem>
-                            ))}
-                        </div>
-                    ) : <div className='hidden'></div>}
-                </Listbox>
+            <div className="mt-4">
+                {results.length > 0 ? (
+                    <div className="bg-white shadow-lg rounded-lg p-4 space-y-2">
+                        {results.map((restaurant) => (
+                            <a
+                                key={restaurant.label}
+                                href={`/${restaurant.label.replace(/\s+/g, '-').toLowerCase()}`}
+                                className="block bg-gray-100 hover:bg-gray-200 transition-colors duration-300 p-2 rounded-md"
+                            >
+                                <div className="flex items-center gap-3">
+                                    <img
+                                        src={restaurant.image}
+                                        alt={restaurant.label}
+                                        className="w-10 h-10 object-cover rounded-full"
+                                    />
+                                    <span className="font-semibold text-gray-800">
+                                        {restaurant.label}
+                                    </span>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
+                ) : <div className='hidden'></div> }
+            </div>
 
-            </ListboxWrapper>
         </div>
     );
 };
