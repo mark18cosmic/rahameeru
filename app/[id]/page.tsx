2ac3209186a3e8f253a762d9a6961675870e5ef3
mark18cosmic <kaish018@gmail.com>
2024-10-08 07:30:57 +0500

    automated commit message

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,4 +1,6 @@
 // Import necessary modules and components
+
+'use client'
 import { Metadata } from 'next';
 import { getRestaurantsData } from '@/app/utils/getRestaurantData';
 import RestaurantDetail from '@/app/components/restaurantDetails';
